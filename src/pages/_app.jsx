d4b87314beef4214b81399b944fdcdc1d1a30e0e
kreiszistvan <kreiszdev@gmail.com>
2020-07-30 03:48:26 +0200

    integrates firebase (analytics)

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,6 +2,22 @@ import './styles/styles.scss'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
+import firebase from 'firebase/app'
+import 'firebase/analytics'
+
+const firebaseConfig = {
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+	measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+}
+
+if (!firebase.apps.length) {
+	firebase.initializeApp(firebaseConfig)
+}
+
 library.add(fab)
 
 export default function App({ Component, pageProps }) {
