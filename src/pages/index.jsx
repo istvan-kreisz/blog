a7b3862d56e70aaddf5b78d1a64bfcd4ce2cca0b
Istvan Kreisz <kreiszdev@gmail.com>
2021-11-28 18:56:48 -0500

    test

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,6 +14,7 @@ const About = ({ title, description }) => {
 						<div className={classes.titlebar}>
 							{/* <h1 className="title">Hi, I'm Istvan!</h1>
 							<h2>iOS and Web Developer</h2> */}
+							<h2>Test</h2>
 						</div>
 
 						<p>
